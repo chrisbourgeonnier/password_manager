d7e8cad71575534d556e319ba1440caad1595c94
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-03-30 11:17:53 +0700

    Password sharing roles and permissions - part 1

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,8 @@
 class PasswordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_password, except: [:index, :new, :create]
+  before_action :require_editable_permission, only: [:edit, :update]
+  before_action :require_deletable_permission, only: [:destroy]
 
   def index
     @passwords = current_user.passwords
@@ -14,24 +16,14 @@ class PasswordsController < ApplicationController
   end
 
   def create
-    @password = current_user.passwords.create(password_params)
-    if @password.persisted?
+    @password = Password.new(password_params)
+    @password.user_passwords.new(user: current_user, role: :owner)
+    if @password.save
       redirect_to @password
     else
       render :new, status: :unprocessable_entity
     end
   end
-
-  # or
-  # def create
-  #   @password = Password.new(password_params)
-  #   @password.user_passwords.new(user: current_user)
-  #   if @password.save
-  #     redirect_to @password
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
 
   def edit
   end
@@ -57,5 +49,14 @@ class PasswordsController < ApplicationController
 
   def set_password
     @password = current_user.passwords.find(params[:id])
+    @user_password = current_user.user_passwords.find_by(password: @password)
+  end
+
+  def require_editable_permission
+    redirect_to @password unless @user_password.editable?
+  end
+
+  def require_deletable_permission
+    redirect_to @password unless @user_password.deletable?
   end
 end
--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,37 @@
 class UserPassword < ApplicationRecord
+  ROLES = %w{viewer editor owner}
+  # Viewer can view the password
+  # Editor can update the password
+  # Owner can view, edit and share the password
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: { in: ROLES }
+
+  attribute :role, default: :viewer
+
+  def owner?
+    role == "owner"
+  end
+
+  def viewer?
+    role == "viewer"
+  end
+
+  def editor?
+    role == "editor"
+  end
+
+  def editable?
+    owner? || editor?
+  end
+
+  def shareable?
+    owner?
+  end
+
+  def deletable?
+    owner?
+  end
 end
